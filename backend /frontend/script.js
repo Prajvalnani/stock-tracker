7026156f2f27a9b5b45f11bbb67f4c7d27b82d35
Prajvalnani <117132866+Prajvalnani@users.noreply.github.com>
2026-04-24 17:27:26 +0530

    Create script.js

---
+++ backend /frontend/script.js
@@ -0,0 +1,15 @@
+const API = "https://YOUR-RAILWAY-URL"; // replace later
+
+async function loadMovers() {
+  const res = await fetch(`${API}/stocks/movers`);
+  const data = await res.json();
+
+  const app = document.getElementById("app");
+
+  app.innerHTML = `
+    <h1>📈 Market Movers</h1>
+    <pre>${JSON.stringify(data, null, 2)}</pre>
+  `;
+}
+
+loadMovers();
